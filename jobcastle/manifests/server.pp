c97ffad547bbb31f4b8097be4faf7fdd1740b00d
Michael Porter <michael@skookum.com>
2015-05-05 22:35:50 -0400

    Only purge yum repos and setup archives for old Fedora

--- jobcastle/manifests/server.pp
+++ jobcastle/manifests/server.pp
@@ -2,19 +2,21 @@ class jobcastle::server {
 
     Exec { path => "/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/kerberos/bin" }
 
-    yumrepo { 'fedora-archives':
-      ensure => present,
-      baseurl => 'http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/$releasever/Everything/$basearch/os/',
-      gpgkey => 'http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/$releasever/Everything/$basearch/os/RPM-GPG-KEY-fedora-$basearch',
-      enabled => true,
-      gpgcheck => true,
-      require => File['/etc/yum.repos.d']
-    }
+    if $operatingsystem == 'Fedora' and $operatingsystemmajrelease < 20 {
+      yumrepo { 'fedora-archives':
+        ensure => present,
+        baseurl => 'http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/$releasever/Everything/$basearch/os/',
+        gpgkey => 'http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/$releasever/Everything/$basearch/os/RPM-GPG-KEY-fedora-$basearch',
+        enabled => true,
+        gpgcheck => true,
+        require => File['/etc/yum.repos.d']
+      }
 
-    file { '/etc/yum.repos.d':
-      ensure => directory,
-      recurse => true,
-      purge => true
+      file { '/etc/yum.repos.d':
+        ensure => directory,
+        recurse => true,
+        purge => true
+      }
     }
 
     package { ['ntp', 'dstat']:
