d30802d643e32a9dc56e51bfe50501b7d6dd5ae1
Michael Porter <michael@skookum.com>
2015-05-07 01:37:05 -0400

    Trim php-pecl-apc as it's not used in recent PHP; ensure Remi repo is installed on EL systems

--- jobcastle/manifests/php.pp
+++ jobcastle/manifests/php.pp
@@ -4,10 +4,29 @@ class jobcastle::php {
     php_enable => true
   }
 
+  case $::operatingsystem {
+    'RedHat', 'CentOS': {
+      package { 'epel-release':
+        ensure => latest
+      }
+      package { 'remi-release':
+        ensure => installed,
+        provider => rpm,
+        source => "http://rpms.famillecollet.com/enterprise/remi-release-$::operatingsystemmajrelease.rpm",
+        require => Package['epel-release']
+      }
+      $package_reqs = [Package['remi-release']]
+    }
+    default: {
+      $package_reqs = []
+    }
+  }
+
   # Yes, this is hardcoded for Fedora / Redhat packages
-  package { ['php-tidy', 'php-pecl-apc', 'php-mbstring', 'php-xml', 'php-pecl-ssh2']:
+  package { ['php-tidy', 'php-mbstring', 'php-xml', 'php-pecl-ssh2']:
     ensure => latest,
-    notify => Service['httpd']
+    notify => Service['httpd'],
+    require => $package_reqs
   }
 
   file { '/etc/php_browscap.ini':
