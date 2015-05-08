5137477aed25fa8a8027783f3212f51d61bdec9e
Michael Porter <michael@skookum.com>
2015-05-07 22:54:41 -0400

    Make installing / configuring MySQL optional

--- jobcastle/manifests/init.pp
+++ jobcastle/manifests/init.pp
@@ -1,17 +1,20 @@
 class jobcastle(
   $db_user      = $jobcastle::params::db_user,
   $db_password  = $jobcastle::params::db_password,
-  $db_name      = $jobcastle::params::db_name
+  $db_name      = $jobcastle::params::db_name,
+  $init_db      = true
 ) inherits jobcastle::params {
 
   class { 'jobcastle::base': }
 
   class { 'jobcastle::php': }
 
-  class { 'jobcastle::db':
-    username    => $db_user,
-    password    => $db_password,
-    schema      => $db_name
+  if $init_db {
+    class { 'jobcastle::db':
+      username    => $db_user,
+      password    => $db_password,
+      schema      => $db_name
+    }
   }
 
 }
